c885e064995f3ada1ecb2b321e493d7976dd9267
derekdogg <135413966+derekdogg@users.noreply.github.com>
2023-06-15 16:31:03 +0930

    Update chunk.pas

--- chunk.pas
+++ chunk.pas
@@ -1,167 +1,60 @@
 unit Chunk;
 
 interface
-uses Loxtypes;
 
-
-(* Taken from crafting interpreters pp398 grow array - this hopefully mimics the c code close enough *)
-
+uses
+  Loxtypes,
+  DoubleArray,
+  IntegerArray,
+  ByteArray;
 
 type
 
-  pSlotType = ^TSlotType;
-  TSlotType = integer;
-
-  TDynamicArray = record
-  const
-    MIN_CAPACITY = 2;
-    INCREMENT_CAPACITY_BY = 2;
+  TChunk = record
   private
-    FResizeCount   : integer;
-    FIndex         : integer;
-    FCount         : integer;
-    FPrevcapacity  : integer;
-    FCapacity      : integer;
-    FItems         : pointer;
-    procedure AllocateArray(var p : pointer; const size : integer);
-    Procedure GrowArray;
-    procedure GrowCapacity;
+    FConstantCount : integer;
+    FOpCodes   : TBytes;
+    FConstants : TDoubles;
+    // FLine      : TIntegers;
   public
-    function Capacity : integer;
-    function IsFull : boolean;
-    function ResizeCount : integer;
-    function FreeSpace : integer;
-    function Count : integer;
-    function Add(const value : TSlotType) : integer;
-    function  Item(const index : integer) : pSlotType;
-    constructor init;
-    procedure finalize; //<-- no destructor allowed, seems weird.
- end;
-
-
- TChunk = record
-   OPCodes      : TDynamicArray;
-   ConstantPool : TDynamicArray;
-   procedure init;
-   procedure finalize;
- end;
+    function  AddConstant(const Value: Double) : Integer;
+    function  AddReturn : integer;
+    procedure init;
+    procedure finalize;
+  end;
 
 
 implementation
 
+uses
+  sysutils;
 
-function TDynamicArray.Capacity : integer;
+
+//add the opcode constant to the opcodes, add a constant to the constants, then add the index of the contant to the opcodes.
+function TChunk.AddConstant(const Value: Double) : Integer;
 begin
-  result := FCapacity;
+  if FConstantCount = high(Byte) then raise exception.create('Max constants reached'); //note since the opcodes is bytes array, it has fixed index size of 256
+  inc(FConstantCount);
+  result := FOpCodes.Add(ord(OP_CONSTANT));
+  FOpCodes.Add(FConstants.Add(Value));
 end;
 
-
-procedure TDynamicArray.AllocateArray(var p : pointer; const size : integer);
+function TChunk.AddReturn : integer;
 begin
-  assert(p = nil);
-  getMem(p,size);
-  fillchar(p^,size,#0);
-
+  result := FOPCodes.Add(ord(OP_RETURN));
 end;
 
-function TDynamicArray.Count: integer;
-begin
-  result := FCount;
-end;
-
-procedure  TDynamicArray.GrowArray;
-var
-  pCopyItems : pointer;
-begin
-  pCopyItems := nil;
-  if not isfull then exit;
-  GrowCapacity;
-  AllocateArray(pCopyItems,FCapacity);
-  Move(FItems^, pCopyItems^, FPrevCapacity);  //copy existing memory into new memory;
-  FreeMem(FItems);                           //free the old memory
-  FItems := nil;                             //make the old memory nil
-  FItems := pCopyItems;                      // set the old memory to the new memory;
-
-  inc(FResizeCount); //<-- used for debug checking.
-end;
-
-
-function TDynamicArray.Add(const value : TSlotType) : integer;
-var
-  pIndex : pSlotType;
-begin
-  result := FCount;
-  GrowArray;
-  pIndex  := Item(FCount);
-  pIndex^ := value;
-  inc(FCount);
-end;
-
-function TDynamicArray.Item(const index: integer): pSlotType;
-begin
-  assert(FCapacity > 0);
-  assert(FItems <> nil);
-  assert((FIndex * sizeof(TSlotType)) <= FCapacity);
-  result := @FItems^;
-  inc(result,index);
-end;
-
-function TDynamicArray.ResizeCount: integer;
-begin
-  result := FResizeCount;
-end;
-
-procedure TDynamicArray.finalize;
-begin
-  if assigned(FItems) then
-  begin
-    freeMem(FItems);
-    FItems := nil;
-  end;
-end;
-
-function TDynamicArray.FreeSpace: integer;
-begin
-  result := 0;
-  if FCapacity = 0 then exit;
-  result := (FCapacity - (FCount * sizeof(TSlotType))) div Sizeof(TSLotType);
-end;
-
-function TDynamicArray.IsFull : boolean;
-begin
-  result := (FCount * sizeof(TSlotType)) = FCapacity
-end;
-
-procedure TDynamicArray.growCapacity;
-begin
-  FPrevCapacity := FCapacity;
-  FCapacity := FCapacity  * INCREMENT_CAPACITY_BY;
-  assert(FCapacity mod sizeof(TSlotType) = 0);
-end;
-
-Constructor TDynamicArray.init;
-begin
-  FresizeCount := 0;
-  FItems := nil;
-  Findex := 0;
-  Fcount := 0;
-  Fcapacity := MIN_CAPACITY * sizeof(TSlotType);
-  Fprevcapacity := Fcapacity;
-
-  AllocateArray(FItems,Fcapacity);
-end;
-
-{ TChunk }
 procedure TChunk.finalize;
 begin
-  OPCodes.Finalize;
-  ConstantPool.Finalize;
+  FOPCodes.Finalize;
+  FConstants.Finalize;
 end;
 
 procedure TChunk.init;
 begin
-   OPCodes.Init;
-   ConstantPool.Init;
+   FOPCodes.Init;
+   FConstants.Init;
 end;
+
 
 end.
