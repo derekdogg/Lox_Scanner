c2baa6841fbee88dd88bad5585e71f5cd63359d9
derekdogg <135413966+derekdogg@users.noreply.github.com>
2023-06-19 20:53:09 +0930

    Create compiler.pas

---
+++ compiler.pas
@@ -0,0 +1,166 @@
+unit compiler;
+
+interface
+
+uses LOXTypes;
+
+
+type
+
+ TParseFn = procedure(const canAssign : boolean) of object;
+
+ TPrecedence = (
+  PREC_NONE,
+  PREC_ASSIGNMENT,  // =
+  PREC_OR,          // or
+  PREC_AND,         // and
+  PREC_EQUALITY,    // == !=
+  PREC_COMPARISON,  // < > <= >=
+  PREC_TERM,        // + -
+  PREC_FACTOR,      // * /
+  PREC_UNARY,       // ! -
+  PREC_CALL,        // . ()
+  PREC_PRIMARY
+ );
+
+
+
+  TParseRule = record
+    prefix: TParseFn;
+    infix: TParseFn;
+    precedence: TPrecedence;
+  end;
+
+  TCompiler = record
+    procedure grouping(const canAssign : boolean);
+    function getRule(TokenKind : TTokenKind) : TParseRule;
+    procedure consume(const Token : TTokenKind; const Message : String);
+    procedure expression;
+    procedure call(const canAssign : boolean);
+  end;
+
+
+implementation
+
+function TCompiler.getRule(TokenKind : TTokenKind) : TParseRule;
+const
+
+  TParseRules: array[tknull..tkthis] of TParseRule = (
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tknull
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkNumber
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkQuotes
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkUnterminatedQuotes
+    (prefix: grouping; infix: call; precedence: PREC_CALL), //tkopen_bracket
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkclose_Bracket
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkAsterisk
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkPlus
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkComma
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkminus
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkBangEqual
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkBang
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkEqual
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkEqualEqual
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkLessThanEqual
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkLess_than
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkGreaterThanEqual
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkgreater_than
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkAnd
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkClass
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkElse
+    (prefix: nil; infix: nil; precedence: PREC_NONE), //tkFalse
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkFun
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkFor
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkIf
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkNil
+    (prefix: nil; infix: nil; precedence: PREC_NONE),// tkOr
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkPrint
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkReturn
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkTrue
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkVar
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkWhile
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkIdentifier
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkWhiteSpace
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkOpenBrace
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkCloseBrace
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkInteger
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkunderscore
+    (prefix: nil; infix: nil; precedence: PREC_NONE),//tkThis
+    (prefix: nil; infix: nil; precedence: PREC_NONE));
+
+begin
+  result :=  TParseRules[tokenKind];
+end;
+
+
+procedure advance;
+begin
+  (*
+  parser.previous = parser.current;
+
+  for (;;) {
+    parser.current = scanToken();
+    if (parser.current.type != TOKEN_ERROR) break;
+
+    errorAtCurrent(parser.current.start);
+  }
+  *)
+end;
+
+
+procedure parsePrecedence(precedence : TPrecedence);
+var
+  prefixRule : TParseFn;
+  canAssign : boolean;
+begin
+  advance();
+  (*
+  prefixRule := getRule(parser.previous.type).prefix;
+  if (prefixRule = nil) then
+  begin
+    error("Expect expression.");
+    exit;
+  end;
+
+  canAssign = precedence <= PREC_ASSIGNMENT;
+  prefixRule(canAssign);
+
+  while (precedence <= getRule(parser.current.type).precedence) do
+  begin
+    advance();
+    ParseFn infixRule = getRule(parser.previous.type)->infix;
+
+    infixRule(canAssign);
+  end;
+
+  if (canAssign && match(TOKEN_EQUAL)) then
+  begin
+    error("Invalid assignment target.");
+  end; *)
+end;
+
+procedure expression;
+begin
+  parsePrecedence(PREC_ASSIGNMENT);
+end;
+
+procedure consume(const Token : TTokenKind; const Message : String);
+begin
+
+end;
+
+procedure grouping(const canAssign : boolean);
+begin
+   expression();
+   consume(tkopen_bracket, 'Expect '')'' after expression.');
+end;
+
+procedure call(const canAssign : boolean);
+begin
+
+end;
+
+
+
+
+
+end.
